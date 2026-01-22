c72e6074c00b8ed29c20df971b6b2fb1a844a898
23f2000592 <23f2000592@ds.study.iitm.ac.in>
2026-01-22 14:24:01 +0530

    feat: implement add_task to append to csv

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
+import sys
+import csv
+
+
+def add_task(task):
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        last_line = lines[-1] if lines else "0"
+        last_id = int(last_line.split(',')[0]) if last_line.strip() != "ID,Task,Status" else 0
+
+    new_id = last_id + 1
+    with open('data.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([new_id, task, 0])
+
+
 if __name__ == "__main__":
-    print("Todo CLI")
+    args = sys.argv[1:]
+
+    if len(args) < 2:
+        sys.exit(1)
+
+    op = args[0]
+    task = args[1]
+
+    if op == "add":
+        add_task(task)
